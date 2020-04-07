2520bd469cb4a783aac2d8000897b1f9751cec9b
Chandan Reddy <chandanreddy@Chandans-MBP.lan>
2020-04-06 21:15:36 -0700

    Second Commit

--- planets.py
+++ planets.py
@@ -3,7 +3,7 @@ def weight_on_planets():
     e_weight = float(e_raw_weight)
     m_weight = e_weight*0.38
     j_weight = e_weight*2.34
-    print("\nOn Mars you would weigh "+str(m_weight)+" pounds.\nOn Jupiter you would weigh "+str(j_weight)+" pounds.")
+    print("\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds." % (m_weight, j_weight))
 
 
 if __name__ == '__main__':
